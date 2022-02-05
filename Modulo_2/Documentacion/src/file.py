747db1de0d47b131a76f9b905bbc2cdd9fcad190
grayTurtle01 <the.correo@protonmail.com>
2022-02-05 17:53:46 -0600

    Modulo-2

---
+++ Modulo_2/Documentacion/src/file.py
@@ -0,0 +1,10 @@
+from datetime import *
+from dateutil.relativedelta import *
+
+# Print date (YYYY-MM-DD)
+now = datetime.now()
+print(now)
+
+# ????
+now = now + relativedelta(months=1, weeks=1, hour=10)
+print(now)
